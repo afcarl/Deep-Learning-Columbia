da16ae24b017890bddb5e4eabc43fdbf69cd97c3
Bahul Jain <bkj2111@columbia.edu>
2016-01-21 10:46:27 -0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,4 +1,18 @@
 import theano
 import theano.tensor as T
 import numpy as np
+from theano.tensor.shared_randomstreams import RandomStreams
+from theano import function
+
+X = T.fmatrix('X')
+
+srng = RandomStreams(seed=234)
+mat = srng.normal((10,10))
+vec = srng.normal((10,1))
+
+a_chng = function([], mat)
+a_fix = function([], mat, no_default_updates=True)
+
+b_chng = function([], vec)
+b_fix = function([], vec, no_default_updates=True)
 
